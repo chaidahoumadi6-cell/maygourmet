54a848cf98fae17020f04b7fd333aebde621c7a7
chaidahoumadi6-cel <chaidahoumadi6@gmail.com>
2026-01-30 09:03:40 +0300

    j'ai créer des vues dans le dossier views en utilisons le moteur EJS pour les vues

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 const express = require('express');
 const app = express();
 
-//  API Route pour la racine de la page : localhost:3004
+// Je précise que les vues sont dans le dossier views
+app.set('views', './views');
+
+// Je précise que nous utilisons le moteur EJS pour les vues
+app.set('view engine', 'ejs');
+
+
+
+
+//  API Route pour la racine de la page : localhost:3004/
 app.get('/', (req, res) => {
     // Message à afficher : Bienvenue chez May Gourmet
     res.write("<h1> Bienvenue chez May Gourmet </h1>");
@@ -13,22 +22,22 @@ app.get('/', (req, res) => {
 app.get("/api/accueil", (req, res) => {
     console.log(" Je passe dans /api/accueil");
 
+    res.render('accueil');
+
     // Le type d'encodage du tex
-    res.writeHead(200,{ "content-type": "text/html;charset=utf-8"})
+    //res.writeHead(200,{ "content-type": "text/html;charset=utf-8"})
 
     // Le conten qui seraa affiché côté navigateur 
-    res.write("<p> Je suis à l'accueil </p>");
+    //res.write("<p> Je suis à l'accueil </p>");
 
     // Fin de la réponse
-    res.end();
+    //res.end();
 });
-
 
 app.get("/api/equipe", (req, res) => {
-  res.write("<p></p>");
-   res.end();
+    console.log(" Je passe dans /api/equipe");
+    res.render('equipe');
 });
-
 
 
 //fin du fichier. Donc ne pas coder en dessous de celui-ci
